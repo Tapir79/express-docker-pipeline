df795c7e7720683a4aa2845c82f8fa1f760d15a1
Tapir79 <saara.m.pakarinen@gmail.com>
2025-05-26 08:17:44 +0300

    Change app message 2nd time

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Updated message from Github and Watchtower!</h1>')
+  res.send('<h1>Updated message 2 from Github and Watchtower!</h1>')
 })
 
 const PORT = 8080
